a6302f6a419bb1a0649ba3c135c936ff5c28a328
unknown <xypnotica@gmail.com>
2019-05-22 08:25:24 +0200

    home page almost done

--- client/src/components/layout/home/Home.js
+++ client/src/components/layout/home/Home.js
@@ -1,21 +1,26 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 // components
-import Logo from "./Logo";
-import Nav from "./Nav";
+import Logo from './Logo';
+import Nav from './Nav';
 
 class Home extends Component {
   render() {
     return (
-      <div className="home">
-        <div className="container">
-          <header className="home__header">
+      <div className='home'>
+        <div className='home__black-mask' />
+        <div className='home__blue-mask' />
+        <div className='container'>
+          <header className='home__header'>
             <Logo />
             <Nav />
           </header>
         </div>
-        <div className="home__message">
-          <span className="home__message--span">Heloo</span>
-          <h1 className="home__message--h1">I`m Developer </h1>
+        <div className='home__message'>
+          <span className='home__message--span'>Heloo</span>
+          <h1 className='home__message--h1'>
+            `m Javascript{' '}
+            <span className='home__message--h1--span'>Developer</span>{' '}
+          </h1>
         </div>
       </div>
     );
--- client/src/components/layout/home/Logo.js
+++ client/src/components/layout/home/Logo.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const Logo = () => {
-  return <div className="home__logo">devxad</div>;
+  return <div className='home__logo'>Devxad</div>;
 };
 
 export default Logo;
