4d465b589513af71faad4d8561f3f4d164f8e9b7
unknown <xypnotica@gmail.com>
2019-05-24 01:17:05 +0200

    about almost done

--- client/src/components/layout/about/About.js
+++ client/src/components/layout/about/About.js
@@ -14,53 +14,83 @@ class About extends Component {
           eget dolor aenean massa eget dolor aenean massa
         </p>
         <div className='about__content'>
-          <div className='about__col-1'>
-            <i className='fab fa-html5' />
-            <h3 className='about__h3'>Html/Css/Sass</h3>
-            <p className='about_col__p'>
-              We at Caliber design and create brilliance at work in a uniquely
-              stylish process.
-            </p>
+          <div className='about__box'>
+            <div className='about__icon--container'>
+              <i className='fab fa-html5 about__icon' />
+            </div>
+            <div className='about__box--container'>
+              <h3 className='about__h3'>Html/Css/Sass</h3>
+              <p className='about__box__p'>
+                We at Caliber design and create brilliance at work in a uniquely
+                stylish process.
+              </p>
+            </div>
           </div>
-          <div className='about__col-2'>
-            <i className='fab fa-js-square' />
-            <h3 className='about__h3p'>Javascript</h3>
-            <p className='about_col__p'>
-              We at Caliber design and create brilliance at work in a uniquely
-              stylish process.
-            </p>
+
+          <div className='about__box'>
+            <div className='about__icon--container'>
+              <i className='fab fa-js-square about__icon' />
+            </div>
+            <div className='about__box--container'>
+              <h3 className='about__h3'>Javascript</h3>
+              <p className='about__box__p'>
+                We at Caliber design and create brilliance at work in a uniquely
+                stylish process.
+              </p>
+            </div>
           </div>
-          <div className='about__col-3'>
-            <i className='fab fa-react' />
-            <h3 className='about__h3'>React/Redux</h3>
-            <p className='about_col__p'>
-              We at Caliber design and create brilliance at work in a uniquely
-              stylish process.
-            </p>
+
+          <div className='about__box'>
+            <div className='about__icon--container'>
+              <i className='fab fa-react about__icon' />
+            </div>
+            <div className='about__box--container'>
+              <h3 className='about__h3'>React/Redux</h3>
+              <p className='about__box__p'>
+                We at Caliber design and create brilliance at work in a uniquely
+                stylish process.
+              </p>
+            </div>
           </div>
-          <div className='about__col-4'>
-            <i className='fab fa-node-js' />
-            <h3 className='about__h3'>Node/Express</h3>
-            <p className='about_col__p'>
-              We at Caliber design and create brilliance at work in a uniquely
-              stylish process.
-            </p>
+
+          <div className='about__box'>
+            <div className='about__icon--container'>
+              <i className='fab fa-node-js about__icon' />
+            </div>
+            <div className='about__box--container'>
+              <h3 className='about__h3'>Node/Express</h3>
+              <p className='about__box__p'>
+                We at Caliber design and create brilliance at work in a uniquely
+                stylish process.
+              </p>
+            </div>
           </div>
-          <div className='about__col-5'>
-            <i className='fas fa-database' />
-            <h3 className='about__h3'>Mongo/Mongoose</h3>
-            <p className='about_col__p'>
-              We at Caliber design and create brilliance at work in a uniquely
-              stylish process.
-            </p>
+
+          <div className='about__box'>
+            <div className='about__icon--container'>
+              <i className='fas fa-database about__icon' />
+            </div>
+            <div className='about__box--container'>
+              <h3 className='about__h3'>Mongo/Mongoose</h3>
+              <p className='about__box__p'>
+                We at Caliber design and create brilliance at work in a uniquely
+                stylish process.
+              </p>
+            </div>
           </div>
-          <div className='about__col-6'>
-            <i class='fas fa-laptop-code' />
-            <h3 className='about__h3'>Other</h3>
-            <p className='about_col__p'>
-              We at Caliber design and create brilliance at work in a uniquely
-              stylish process.
-            </p>
+
+          <div className='about__box'>
+            <div className='about__icon--container'>
+              {/* <i className='fas fa-laptop-code about__icon' /> */}
+              <i className='fas fa-skiing about__icon' />
+            </div>
+            <div className='about__box--container'>
+              <h3 className='about__h3'>Other</h3>
+              <p className='about__box__p'>
+                We at Caliber design and create brilliance at work in a uniquely
+                stylish process.
+              </p>
+            </div>
           </div>
         </div>
       </div>
