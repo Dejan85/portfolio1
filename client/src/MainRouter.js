c73dca6646e073b6e3840c2526ad5d54dfd7802f
unknown <xypnotica@gmail.com>
2019-05-24 02:43:28 +0200

    services component created

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -4,12 +4,14 @@ import { Route } from 'react-router-dom';
 // components
 import Home from './components/layout/home/Home';
 import About from './components/layout/about/About';
+import Services from './components/layout/services/Services';
 
 const Router = () => {
   return (
     <>
       <Route exact to='/' component={Home} />
       <Route exact to='/' component={About} />
+      <Route exact to='/' component={Services} />
     </>
   );
 };
---
+++ client/src/components/layout/services/Services.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Services extends Component {
+  render() {
+    return <div className='services'>services</div>;
+  }
+}
+
+export default Services;
