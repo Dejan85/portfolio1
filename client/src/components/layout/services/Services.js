362dc566a4a28a3cfac833a37af7693b5e8df1e8
unknown <xypnotica@gmail.com>
2019-05-24 04:04:19 +0200

    services page

--- client/src/components/layout/services/Services.js
+++ client/src/components/layout/services/Services.js
@@ -1,8 +1,60 @@
 import React, { Component } from 'react';
 
+// components
+import Line from '../utilities/Line';
+
 class Services extends Component {
   render() {
-    return <div className='services'>services</div>;
+    return (
+      <div className='services'>
+        <div className='services__content'>
+          <div className='services__h2--container'>
+            <h2 className='services__h2'>Art of work</h2>
+            <Line />
+          </div>
+
+          <div className='services__box'>
+            <div className='services__icon'>
+              <i className='fas fa-desktop' />
+            </div>
+            <div className='services__box__container'>
+              <h3 className='services__h3'>Static Page</h3>
+              <p className='services__p'>
+                We at Caliber design and create brilliance at work in a uniquely
+                stylish process.
+              </p>
+            </div>
+          </div>
+
+          <div className='services__box'>
+            <div className='services__icon'>
+              <i className='fas fa-desktop' />
+            </div>
+            <div className='services__box__container'>
+              <h3 className='services__h3'>Web App</h3>
+              <p className='services__p'>
+                We at Caliber design and create brilliance at work in a uniquely
+                stylish process.
+              </p>
+            </div>
+          </div>
+
+          <div className='services__box'>
+            <div className='services__icon'>
+              <i className='fas fa-desktop' />
+            </div>
+            <div className='services__box__container'>
+              <h3 className='services__h3'>Fullstack </h3>
+              <p className='services__p'>
+                We at Caliber design and create brilliance at work in a uniquely
+                stylish process.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='services__decoration-div' />
+      </div>
+    );
   }
 }
 
