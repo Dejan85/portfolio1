9e2b9c020e1f8f3dd24cc14e8ce3391899b09518
unknown <xypnotica@gmail.com>
2019-05-29 04:56:25 +0200

    contact page almost done

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
 
 // components
-import Home from './components/layout/home/Home';
-import About from './components/layout/about/About';
-import Services from './components/layout/services/Services';
-import Works from './components/layout/works/Works';
-import Skills from './components/layout/skills/Skills';
-import Portfolio from './components/layout/portfolio/Portfolio';
-import Blog from './components/layout/blog/Blog';
-import Social from './components/layout/social/Social';
+import Home from "./components/layout/home/Home";
+import About from "./components/layout/about/About";
+import Services from "./components/layout/services/Services";
+import Works from "./components/layout/works/Works";
+import Skills from "./components/layout/skills/Skills";
+import Portfolio from "./components/layout/portfolio/Portfolio";
+import Blog from "./components/layout/blog/Blog";
+import Social from "./components/layout/social/Social";
+import Contact from "./components/layout/contact/Contact";
 
 const Router = () => {
   return (
     <>
-      <Route exact to='/' component={Home} />
-      <Route exact to='/' component={About} />
-      <Route exact to='/' component={Services} />
-      <Route exact to='/' component={Works} />
-      <Route exact to='/' component={Skills} />
-      <Route exact to='/' component={Portfolio} />
-      <Route exact to='/' component={Social} />
-      <Route exact to='/' component={Blog} />
+      <Route exact to="/" component={Home} />
+      <Route exact to="/" component={About} />
+      <Route exact to="/" component={Services} />
+      <Route exact to="/" component={Works} />
+      <Route exact to="/" component={Skills} />
+      <Route exact to="/" component={Portfolio} />
+      <Route exact to="/" component={Social} />
+      <Route exact to="/" component={Blog} />
+      <Route exact to="/" component={Contact} />
     </>
   );
 };
---
+++ client/src/components/layout/contact/Contact.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+import avatar from "../../../sass/images/avatar.PNG";
+
+// component
+import Line from "../utilities/Line";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="contact">
+        <h2 className="contact__h2">Contact Me</h2>
+        <Line />
+        <p className="contact__p">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit enean commodo
+          eget dolor aenean massa eget dolor aenean massa
+        </p>
+
+        <div className="contact__content container">
+          <div className="contact__image">
+            <img className="contact__image--img" alt="slika" src={avatar} />
+          </div>
+          <div className="contact__form">
+            <form>
+              <div className="contact__input-holder">
+                <input placeholder="Your Name" />
+              </div>
+              <div className="contact__input-holder">
+                <input placeholder="Your Email" />
+              </div>
+              <div className="contact__input-holder">
+                <input placeholder="Subject" />
+              </div>
+              <div className="contact__input-holder">
+                <textarea rows="10">Message</textarea>
+              </div>
+              <div className="contact__input-holder">
+                <button className="contact__btn">Send Message</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contact;
