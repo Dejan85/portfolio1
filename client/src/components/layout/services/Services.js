c888ec5f82d1e716cd756c69d60d4dabe01112f6
unknown <xypnotica@gmail.com>
2019-06-01 06:26:13 +0200

    about responsive

--- client/src/components/layout/services/Services.js
+++ client/src/components/layout/services/Services.js
@@ -7,53 +7,55 @@ class Services extends Component {
   render() {
     return (
       <div className="services">
-        <div className="services__content">
-          <div className="services__h2--container">
-            <h2 className="services__h2">Art of work</h2>
+        <div className="services__left-box">
+          <div className="services__content">
+            <h2 className="services__h2">ART OF WORK</h2>
             <Line />
-          </div>
 
-          <div className="services__box">
-            <div className="services__icon">
-              <i className="fas fa-pager" />
+            <div className="services__box">
+              <div className="services__icon">
+                <i className="fas fa-pager" />
+              </div>
+              <div className="services__box__container">
+                <h3 className="services__h3">Static Page</h3>
+                <p className="services__p">
+                  We at Caliber design and create brilliance at work in a
+                  uniquely stylish process.
+                </p>
+              </div>
             </div>
-            <div className="services__box__container">
-              <h3 className="services__h3">Static Page</h3>
-              <p className="services__p">
-                We at Caliber design and create brilliance at work in a uniquely
-                stylish process.
-              </p>
-            </div>
-          </div>
 
-          <div className="services__box">
-            <div className="services__icon">
-              {/* <i className='fas fa-desktop' /> */}
-              <i className="fas fa-window-maximize" />
+            <div className="services__box">
+              <div className="services__icon">
+                {/* <i className='fas fa-desktop' /> */}
+                <i className="fas fa-window-maximize" />
+              </div>
+              <div className="services__box__container">
+                <h3 className="services__h3">Web App</h3>
+                <p className="services__p">
+                  We at Caliber design and create brilliance at work in a
+                  uniquely stylish process.
+                </p>
+              </div>
             </div>
-            <div className="services__box__container">
-              <h3 className="services__h3">Web App</h3>
-              <p className="services__p">
-                We at Caliber design and create brilliance at work in a uniquely
-                stylish process.
-              </p>
-            </div>
-          </div>
 
-          <div className="services__box">
-            <div className="services__icon">
-              <i className="far fa-object-group" />
-            </div>
-            <div className="services__box__container">
-              <h3 className="services__h3">Fullstack </h3>
-              <p className="services__p">
-                We at Caliber design and create brilliance at work in a uniquely
-                stylish process.
-              </p>
+            <div className="services__box">
+              <div className="services__icon">
+                <i className="far fa-object-group" />
+              </div>
+              <div className="services__box__container">
+                <h3 className="services__h3">Fullstack </h3>
+                <p className="services__p">
+                  We at Caliber design and create brilliance at work in a
+                  uniquely stylish process.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="services__decoration-div" />
+        <div className="services__right-box">
+          <div className="services__decoration-div" />
+        </div>
       </div>
     );
   }
