f6bf565a978d4c0907ad72429f1ca71d9ce44cad
unknown <xypnotica@gmail.com>
2019-06-02 14:05:36 +0200

    react scroll on drompdown menu

--- client/src/components/layout/home/DropMenu.js
+++ client/src/components/layout/home/DropMenu.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const DropMenu = () => {
   const [transition, setTransition] = useState({});
+  const { duration } = useState(500);
 
   useEffect(() => {
     setTransition({
@@ -15,42 +16,98 @@ const DropMenu = () => {
     <div className="drop__menu" style={transition}>
       <ul className="drop__menu__list">
         <li className="drop__menu__item">
-          <Link className="drop__menu__link" to="/">
+          <Link
+            className="drop__menu__link"
+            activeClass="active"
+            to="home"
+            spy={true}
+            smooth={"true"}
+            // offset={-400}
+            duration={duration}>
             Home
           </Link>
         </li>
         <li className="drop__menu__item">
-          <Link className="drop__menu__link" to="/">
+          <Link
+            className="drop__menu__link"
+            activeClass="active"
+            to="about"
+            spy={true}
+            smooth={"true"}
+            // offset={-400}
+            duration={duration}>
             About
           </Link>
         </li>
         <li className="drop__menu__item">
-          <Link className="drop__menu__link" to="/">
+          <Link
+            className="drop__menu__link"
+            activeClass="active"
+            to="services"
+            spy={true}
+            smooth={"true"}
+            // offset={-400}
+            duration={duration}>
             Services
           </Link>
         </li>
         <li className="drop__menu__item">
-          <Link className="drop__menu__link" to="/">
-            Team
+          <Link
+            className="drop__menu__link"
+            activeClass="active"
+            to="works"
+            spy={true}
+            smooth={"true"}
+            // offset={-400}
+            duration={duration}>
+            Popular
           </Link>
         </li>
         <li className="drop__menu__item">
-          <Link className="drop__menu__link" to="/">
+          <Link
+            className="drop__menu__link"
+            activeClass="active"
+            to="skills"
+            spy={true}
+            smooth={"true"}
+            // offset={-400}
+            duration={duration}>
             Skills
           </Link>
         </li>
         <li className="drop__menu__item">
-          <Link className="drop__menu__link" to="/">
+          <Link
+            className="drop__menu__link"
+            activeClass="active"
+            to="portfolio"
+            spy={true}
+            smooth={"true"}
+            // offset={-400}
+            duration={duration}>
             Portfolio
           </Link>
         </li>
         <li className="drop__menu__item">
-          <Link className="drop__menu__link" to="/">
+          <Link
+            className="drop__menu__link"
+            activeClass="active"
+            to="blog"
+            spy={true}
+            smooth={"true"}
+            // offset={-400}
+            duration={duration}>
             Blog
           </Link>
         </li>
         <li className="drop__menu__item">
-          <Link className="drop__menu__link" to="/">
+          <Link
+            className="drop__menu__link"
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={"true"}
+            // offset={-400}
+            duration={duration}>
             Contact
           </Link>
         </li>
