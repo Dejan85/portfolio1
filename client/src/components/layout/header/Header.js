bdbd8e4c2219f77cedb61ba10b7d245af4606be8
unknown <xypnotica@gmail.com>
2019-06-03 16:55:02 +0200

    working on drop menu

--- client/src/components/layout/header/Header.js
+++ client/src/components/layout/header/Header.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect, useRef } from "react";
 import Logo from "./Logo";
 import Nav from "./Nav";
 import NavRightSide from "./NavRightSide";
+import DropMenu from "../home/DropMenu";
 
 const Header = () => {
   const [active, setActive] = useState(false);
@@ -16,39 +17,69 @@ const Header = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      setStyle(() => {
-        return window.pageYOffset > headerRef.current.offsetTop
-          ? {
-              header: {
-                position: "fixed",
-                paddingTop: "0rem"
-              },
-              leftSide: {
-                background: "#29b6f6"
-              },
-              rightSide: {
-                background: "#212121"
-              },
-              logo: {
-                color: "#212121",
-                fontSize: "1.6rem"
+      if (window.screen.availWidth > 980) {
+        setStyle(() => {
+          return window.pageYOffset > headerRef.current.offsetTop
+            ? {
+                header: {
+                  position: "fixed",
+                  paddingTop: "0rem"
+                },
+                leftSide: {
+                  background: "#29b6f6"
+                },
+                rightSide: {
+                  background: "#212121"
+                },
+                logo: {
+                  color: "#212121"
+                  // fontSize: "1.6rem"
+                }
               }
-            }
-          : {};
-      });
+            : {};
+        });
+      } else {
+        setStyle(() => {
+          return window.pageYOffset > headerRef.current.offsetTop
+            ? {
+                header: {
+                  position: "fixed",
+                  paddingTop: "0rem",
+                  width: "100%"
+                },
+                leftSide: {
+                  background: "#29b6f6"
+                },
+                rightSide: {
+                  display: "none"
+                },
+                logo: {
+                  // color: "#212121",
+                  fontSize: "2.4rem"
+                }
+              }
+            : {};
+        });
+      }
     });
   }, []);
 
   return (
-    <header className="header" ref={headerRef} style={style.header}>
-      <div className="header__left" style={style.leftSide}>
-        <Logo style={style.logo} />
-        <Nav />
-      </div>
-      <div className="header__right" style={style.rightSide}>
-        <NavRightSide />
-      </div>
-    </header>
+    <>
+      <header className="header" ref={headerRef} style={style.header}>
+        {active ? <DropMenu /> : null}
+        <div className="header__left" style={style.leftSide}>
+          <Logo style={style.logo} />
+          <Nav />
+          <div id="nav-toggle" className={active ? "active" : ""}>
+            <span onClick={handleDropMenu} />
+          </div>
+        </div>
+        <div className="header__right" style={style.rightSide}>
+          <NavRightSide />
+        </div>
+      </header>
+    </>
   );
 };
 
--- client/src/components/layout/header/NavRightSide.js
+++ client/src/components/layout/header/NavRightSide.js
@@ -5,54 +5,52 @@ const NavRightSide = () => {
   const { duration } = useState(500);
 
   return (
-    <nav className="header__nav">
-      <ul className="header__list">
-        <li className="header__item">
-          <Link
-            className="header__link header__link--right"
-            activeClass="active"
-            to="skills"
-            spy={true}
-            smooth={"true"}
-            duration={duration}>
-            Skills
-          </Link>
-        </li>
-        <li className="header__item">
-          <Link
-            className="header__link header__link--right"
-            activeClass="active"
-            to="portfolio"
-            spy={true}
-            smooth={"true"}
-            duration={duration}>
-            Portoflio
-          </Link>
-        </li>
-        <li className="header__item">
-          <Link
-            className="header__link header__link--right"
-            activeClass="active"
-            to="blog"
-            spy={true}
-            smooth={"true"}
-            duration={duration}>
-            Blog
-          </Link>
-        </li>
-        <li className="header__item">
-          <Link
-            className="header__link header__link--right"
-            activeClass="active"
-            to="contact"
-            spy={true}
-            smooth={"true"}
-            duration={duration}>
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <ul className="header__list">
+      <li className="header__item">
+        <Link
+          className="header__link header__link--right"
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={"true"}
+          duration={duration}>
+          Skills
+        </Link>
+      </li>
+      <li className="header__item">
+        <Link
+          className="header__link header__link--right"
+          activeClass="active"
+          to="portfolio"
+          spy={true}
+          smooth={"true"}
+          duration={duration}>
+          Portoflio
+        </Link>
+      </li>
+      <li className="header__item">
+        <Link
+          className="header__link header__link--right"
+          activeClass="active"
+          to="blog"
+          spy={true}
+          smooth={"true"}
+          duration={duration}>
+          Blog
+        </Link>
+      </li>
+      <li className="header__item">
+        <Link
+          className="header__link header__link--right"
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={"true"}
+          duration={duration}>
+          Contact
+        </Link>
+      </li>
+    </ul>
   );
 };
 
