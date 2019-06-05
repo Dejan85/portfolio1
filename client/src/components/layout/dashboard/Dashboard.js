1b5ea1d98465643c115a855f6c2fd11dea94a432
unknown <xypnotica@gmail.com>
2019-06-05 05:09:20 +0200

    start building dashboard

--- client/src/components/layout/dashboard/Dashboard.js
+++ client/src/components/layout/dashboard/Dashboard.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="dashboard">
+      <div className="dashboard__header">
+        <div className="dashboard__logo">Devxad</div>
+        <div className="dashboard__header-nav">Header nav</div>
+      </div>
+      <div className="dashboard__content">
+        <div className="dashboard__nav">nav</div>
+        <div className="dashboard__content-side">content</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
