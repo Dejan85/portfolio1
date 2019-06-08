7921b698862dd1fa876956bd167fa185eaad4f92
unknown <xypnotica@gmail.com>
2019-06-08 06:20:06 +0200

    working on dashboard edit account

--- client/src/components/layout/dashboard/EditAccount.js
+++ client/src/components/layout/dashboard/EditAccount.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 const EditAccount = () => {
-  return <div className="edit-account">edit account</div>;
+  return (
+    <div className="edit-account">
+      <form className="edit-account__form">
+        <div className="edit-account__input-holder">
+          <label className="edit-account__label">Name</label>
+          <input className="edit-account__input" />
+        </div>
+        <div className="edit-account__input-holder">
+          <label className="edit-account__label">Email</label>
+          <input className="edit-account__input" />
+        </div>
+        <div className="edit-account__input-holder">
+          <label className="edit-account__label">Password</label>
+          <input className="edit-account__input" />
+        </div>
+        <div className="edit-account__input-holder">
+          <label className="edit-account__label">Avatar</label>
+          <input className="edit-account__input" />
+        </div>
+        <div className="edit-account__button-holder">
+          <button className="edit-account__button">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default EditAccount;
