7cc3fd21b1f0195b88aec9195b1456ff07016312
unknown <xypnotica@gmail.com>
2019-06-08 19:24:21 +0200

    dashboard delete account almost done

--- client/src/components/layout/dashboard/DeleteAccount.js
+++ client/src/components/layout/dashboard/DeleteAccount.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+// image
+import avatar from "../../../sass/images/avatar.jpg";
+
 const DeleteAccount = () => {
-  return <div className="delete-account">delete account</div>;
+  return (
+    <div className="delete-account">
+      <img className="delete-account__avatar" src={avatar} alt="slika" />
+      <div className="delete-account__user">
+        <p className="delete-account__name">Xypnotica@gmail.com</p>
+        <p className="delete-account__message">
+          Ako stvarno obrises account vise neces moci da ga vratis i trajno ces
+          ga izgubiti. Da li zaista zelis da obrises account?
+        </p>
+        <button className="delete-account__button">Delete</button>
+      </div>
+    </div>
+  );
 };
 
 export default DeleteAccount;
