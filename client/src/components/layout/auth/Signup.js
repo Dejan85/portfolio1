8bb0bedce411e693a7fd952f9d635a9035a7422b
unknown <xypnotica@gmail.com>
2019-06-10 10:49:03 +0200

    start bulding user registration

--- client/src/components/layout/auth/Signup.js
+++ client/src/components/layout/auth/Signup.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [name, setName] = useState("");
+
+  const handlechange = e => {
+    // setInput(console.log(input));
+    // console.log(e.target.name);
+    // console.log(e.target.value);
+  };
+
   return (
     <div className="login">
       <div className="login__content">
@@ -9,22 +17,47 @@ const Signup = () => {
         <form className="login__form">
           <div className="login__input-holder">
             <label className="login__label">Name:</label>
-            <input className="login__input" />
+            <input
+              className="login__input"
+              onChange={e => {
+                setName(e.target.value);
+              }}
+              value={name}
+              name="name"
+            />
           </div>
-          <div className="login__input-holder">
+          {/* <div className="login__input-holder">
             <label className="login__label">Email:</label>
-            <input className="login__input" type="email" />
+            <input
+              className="login__input"
+              type="email"
+              onChange={handlechange}
+              value={input.email}
+              name="email"
+            />
           </div>
 
           <div className="login__input-holder">
             <label className="login__label">Password:</label>
-            <input className="login__input" type="password" />
+            <input
+              className="login__input"
+              type="password"
+              onChange={handlechange}
+              value={input.password}
+              name="password"
+            />
           </div>
 
           <div className="login__input-holder">
             <label className="login__label">Confirm Password:</label>
-            <input className="login__input" type="password" />
-          </div>
+            <input
+              className="login__input"
+              type="password"
+              onChange={handlechange}
+              value={input.confirmPassword}
+              name="confirmPassword"
+            />
+          </div> */}
 
           <div className="login__button-holder">
             <button className="login__button">Registruj se</button>
---
+++ client/src/redux/actions/auth/authAction.js
@@ -0,0 +1,1 @@
+import { SIGNUP } from "../../type/auth/authTypes";
