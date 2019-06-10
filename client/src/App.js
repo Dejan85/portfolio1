5b9b3e5fd1cd605837d27e5e44a6b830064ad6ca
unknown <xypnotica@gmail.com>
2019-06-10 09:50:15 +0200

    start adding redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,25 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import "../src/sass/main.scss";
 
-// components
+// ─── COMPONENTS ─────────────────────────────────────────────────────────────────
+//
+
 import MainRouter from "./MainRouter";
+
+//
+// ─── REUDX ──────────────────────────────────────────────────────────────────────
+//
+
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <Router>
-      <MainRouter />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <MainRouter />
+      </Router>
+    </Provider>
   );
 }
 
