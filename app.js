2a9710d97b2874544ae8919047ea0a11e33524e7
unknown <xypnotica@gmail.com>
2019-06-10 09:26:28 +0200

    auth controllers and routes done

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
 const port = process.env.PORT || 5000;
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const expressValidator = require("express-validator");
+const cookieParser = require("cookie-parser");
+const fs = require("fs");
 dotenv.config();
+
+//
+// ─── DB ─────────────────────────────────────────────────────────────────────────
+//
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => {
+  console.log("Db connected");
+});
+
+mongoose.connection.on("error", err => {
+  console.log(`DB connection error: ${err.message}`);
+});
 
 //
 // ─── MIDDLEWARE ─────────────────────────────────────────────────────────────────
@@ -15,6 +32,26 @@ app.use(morgan("dev"));
 // body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+// cooke parser
+app.use(cookieParser());
+// express validator
+app.use(expressValidator());
+
+//
+// ─── ROUTES ─────────────────────────────────────────────────────────────────────
+//
+
+const auth = require("./nodeapi/routes/auth");
+app.use("/", auth);
+
+// express jwt (this is must be below route)
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({
+      err: "Unauthorized!"
+    });
+  }
+});
 
 //
 // ─── THIS IS SETUP FOR HEROKU DEPLOY ────────────────────────────────────────────
